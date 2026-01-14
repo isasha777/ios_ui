041fa3c81cde26a67fb2ef1ba0d1dd302dba4155
Alex Nekrasow <isasha777@gmail.com>
2026-01-14 14:24:17 +0300

    Move Post to StorageService framework and add Release scheme

--- Navigation1/SceneDelegate.swift
+++ Navigation1/SceneDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import StorageService
 
 final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
---
+++ StorageService/StorageService.swift
@@ -0,0 +1,9 @@
+//
+//  StorageService.swift
+//  StorageService
+//
+//  Created by Alex Nekrasow on 14.01.2026.
+//
+
+import Foundation
+
